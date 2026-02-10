17da4c3a42b1294c3f16d90edec6d104556aa9de
187J3X1-114514 <smjwoaini@qq.com>
2026-02-10 14:54:25 +0800

    修复自动构建错误x4 (SKIP)

--- native/cpp/build_linux.sh
+++ native/cpp/build_linux.sh
@@ -1,7 +1,7 @@
 rm -rf buildLinux
-cmake -S . -B buildLinux -DCMAKE_BUILD_TYPE=Debug -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=OFF
+cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Debug -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=OFF
 cmake --build buildLinux --config Debug -- -j8
 
 rm -rf buildLinux
-cmake -S . -B buildLinux -DCMAKE_BUILD_TYPE=Release -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=OFF
+cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Release -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=OFF
 cmake --build buildLinux --config Release -- -j8
