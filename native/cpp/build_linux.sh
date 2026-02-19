e5771489e1616e76604b8daa58da947b7166bb95
楠ちゃん <kusu@kusu.moe>
2026-02-19 15:14:53 +0800

    [Feat] Add -flto.

--- native/cpp/build_linux.sh
+++ native/cpp/build_linux.sh
@@ -1,7 +1,15 @@
+set -e
+
+export CC=clang
+export CXX=clang++
+
+echo "[native] using C compiler: ${CC}"
+echo "[native] using CXX compiler: ${CXX}"
+
 rm -rf buildLinux
-cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Debug -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
+cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
 cmake --build buildLinux --config Debug -- -j$(nproc)
 
 rm -rf buildLinux
-cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Release -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
+cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
 cmake --build buildLinux --config Release -- -j$(nproc)
