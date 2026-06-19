c60ea2fd74604e989655b83cd0ecdce634db64f3
187J3X1-114514 <smjwoaini@qq.com>
2026-06-19 12:38:35 +0800

    enable FSR native building on linux

--- native/cpp/build_linux.sh
+++ native/cpp/build_linux.sh
@@ -7,9 +7,9 @@ echo "[native] using C compiler: ${CC}"
 echo "[native] using CXX compiler: ${CXX}"
 
 rm -rf buildLinux
-cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSR_FSR=OFF -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
+cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Debug -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
 cmake --build buildLinux --config Debug -- -j$(nproc)
 
 rm -rf buildLinux
-cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSR_FSR=OFF -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
+cmake -G "Ninja" -S . -B buildLinux -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=${CC} -DCMAKE_CXX_COMPILER=${CXX} -DSR_FSR=ON -DSR_XESS=OFF -DSR_FSROGL=OFF -DSR_DLSS=ON
 cmake --build buildLinux --config Release -- -j$(nproc)
